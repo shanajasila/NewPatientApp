802cd7f96545e0b3287a50513353796d1566f9a2
shanajasila <shanajasilam@gmail.com>
2022-12-07 05:52:26 +0530

    view

---
+++ src/app/view-patient/view-patient.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-patient',
+  templateUrl: './view-patient.component.html',
+  styleUrls: ['./view-patient.component.css']
+})
+export class ViewPatientComponent {
+
+  patient:any=[]
+}
